4f1480895f3303a03b96d8ea2af267a7f9a232ef
Bryan Alexander <bryanalexander@me.com>
2015-10-20 11:43:54 -1000

    initial routing completed. need database info to continue

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require('express');
 var app = express();
 
+//denote jade as view engine and view location
 app.set('view engine', 'jade');
 app.set('views', './views');
 
+//defer all gallery routes to gallery router
+var gallery = require('./routes/gallery');
+app.use('/gallery', gallery);
+
+//look to public for static pages
 app.use(express.static('public'));
 
+//where index is located
 app.get('/', function(req, res) {
 
   res.render('index');
 
 });
 
+//start up the server & define port
 var server = app.listen(3000, function() {
 
   var host = server.address().address;
