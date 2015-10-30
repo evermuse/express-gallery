bfff309033c81b3780b8fe3274406b681e02d2b4
Bryan Alexander <bryanalexander@me.com>
2015-10-29 20:29:20 -1000

    added User model

---
+++ models/User.js
@@ -0,0 +1,27 @@
+module.exports = function(sequelize, DataTypes) {
+
+  // define new table entry for posting
+  var User = sequelize.define('users', {
+
+    username : { type : DataTypes.STRING, unique : true },
+    password : DataTypes.STRING
+
+  }, {
+
+    instanceMethods : {
+
+      validPassword : function (password) {
+
+        return (password === this.password);
+
+      }
+
+    },
+
+    freezeTableName: true
+
+  });
+
+  return User;
+
+};
